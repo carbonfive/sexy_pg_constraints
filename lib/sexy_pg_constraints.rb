9eab96b2b0e3a9fcca55a882183f95dc1b0b27ba
Ben Woosley <ben.woosley@gmail.com>
2012-05-03 01:46:07 -0700

    WIP: with thanks to matthuggins/foreigner

--- lib/sexy_pg_constraints.rb
+++ lib/sexy_pg_constraints.rb
@@ -1,6 +1,7 @@
 module SexyPgConstraints
   extend ActiveSupport::Autoload
   autoload :SchemaDefinitions
+  autoload :SchemaDumper
   autoload :Constrainer
   autoload :Deconstrainer
   autoload :Constraints
